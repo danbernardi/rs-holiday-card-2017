e2f9c29f8b154ffb07eb1049713a5139215b705f
Daniel <danbernardi1@gmail.com>
2017-12-11 11:34:04 -0800

    Adds working carousel prototype

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,16 @@
 $(document).ready(function () {
-  console.log('hello there');
-  debugger;
+  var $carousel = $('.carousel').flickity({
+    cellAlign: 'center',
+    contain: false,
+    cellSelector: '.member',
+    percentPosition: true,
+    prevNextButtons: false,
+    pageDots: false,
+    selectedAttraction: 0.01,
+    friction: 0.15
+  });
+
+  $('.carousel').on('staticClick.flickity', function( event, pointer, cellElement, cellIndex ) {
+    $carousel.flickity('select', cellIndex);
+  });
 });
