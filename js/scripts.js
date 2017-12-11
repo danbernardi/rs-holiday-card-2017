89934cd890e4ef11b4bb76cf6c9a1b8bf2a99097
Daniel <danbernardi1@gmail.com>
2017-12-11 12:27:09 -0800

    Adds parallax prototype

--- js/scripts.js
+++ js/scripts.js
@@ -10,7 +10,26 @@ $(document).ready(function () {
     friction: 0.15
   });
 
-  $('.carousel').on('staticClick.flickity', function( event, pointer, cellElement, cellIndex ) {
+  $carousel.on('staticClick.flickity', function( event, pointer, cellElement, cellIndex ) {
     $carousel.flickity('select', cellIndex);
+  });
+
+  var cellRatio = 0.3333333, // outerWidth of cell / width of carousel
+        bgRatio = 0.15, // width of background layer / width of carousel
+        fgRatio = 1.25, // width of foreground layer / width of carousel
+        count = $('.member').length;
+
+  function moveParallaxLayer( $layer, layerRatio, progress ) {
+    console.log(progress);
+    var ratio = cellRatio * layerRatio;
+    $layer.css({
+      left: ( 50% - 0.5 - ( 0.5 + progress * count ) * ratio ) * 100 + '%'
+    });
+  }
+
+  $carousel.on('scroll.flickity', function (event, progress) {
+    console.log(progress);
+
+    moveParallaxLayer($('.background1'), bgRatio, progress);
   });
 });
