d4d7ba6f4302a6151e531b213724964ce0164da1
Daniel <danbernardi1@gmail.com>
2017-12-12 10:54:18 -0800

    Adds bookends / comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,9 @@
 $(document).ready(function () {
+  // initialize carousel
   var $carousel = $('.carousel').flickity({
     cellAlign: 'center',
     contain: false,
-    cellSelector: '.member',
+    cellSelector: '.slide',
     percentPosition: true,
     prevNextButtons: false,
     pageDots: false,
@@ -10,15 +11,21 @@ $(document).ready(function () {
     friction: 0.15
   });
 
+  // force carousel focus on page load
+  $carousel.focus();
+
+  // click handler for each slide
   $carousel.on('staticClick.flickity', function( event, pointer, cellElement, cellIndex ) {
     $carousel.flickity('select', cellIndex);
   });
 
+  // define parallax variables
   var cellRatio = 0.3333333, // outerWidth of cell / width of carousel
         bgRatio = 0.15, // width of background layer / width of carousel
         fgRatio = 1.25, // width of foreground layer / width of carousel
-        count = $('.member').length;
+        count = $('.slide').length;
 
+  // handle parallax animation
   function moveParallaxLayer( $layer, layerRatio, progress ) {
     var ratio = cellRatio * layerRatio;
     $layer.css({
@@ -26,7 +33,13 @@ $(document).ready(function () {
     });
   }
 
+  // trigger parallax animation on scroll
   $carousel.on('scroll.flickity', function (event, progress) {
     moveParallaxLayer($('.background1'), bgRatio, progress);
+  });
+
+  // add click handler for back to start button
+  $('.js-startTrigger').on('click', function () {
+    $carousel.flickity('select', 0);
   });
 });
