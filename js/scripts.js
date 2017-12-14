d91807fbd4ee3e02bf279eda7bc4ba9b53e68efa
Daniel <danbernardi1@gmail.com>
2017-12-14 15:40:19 -0800

    Adds scroll trigger

--- js/scripts.js
+++ js/scripts.js
@@ -42,6 +42,20 @@ $(document).ready(function () {
   $('.js-startTrigger').on('click', function () {
     $carousel.flickity('select', 0);
   });
+
+  var nextSlide = debounce(function () { $carousel.flickity('next'); }, 250, true);
+  var previousSlide = debounce(function () { $carousel.flickity('previous'); }, 250, true);
+
+  $(window).on('mousewheel', function (event) {
+    const xDelta = event.originalEvent.wheelDeltaX;
+    const yDelta = event.originalEvent.wheelDeltaY;
+
+    if (xDelta < -30 || yDelta < -30) {
+      nextSlide();
+    } else if (xDelta > 30 || yDelta > 30) {
+      previousSlide();
+    }
+  })
 });
 
 $(window).on('load', function () {
@@ -52,4 +66,21 @@ $(window).on('load', function () {
   });
 })
 
-
+// Returns a function, that, as long as it continues to be invoked, will not
+// be triggered. The function will be called after it stops being called for
+// N milliseconds. If `immediate` is passed, trigger the function on the
+// leading edge, instead of the trailing.
+function debounce(func, wait, immediate) {
+  var timeout;
+  return function() {
+    var context = this, args = arguments;
+    var later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+};
